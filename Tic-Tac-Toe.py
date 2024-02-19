6327fcab5ad2394a9fb9527694190b50e27f5a1d
Alextay04 <sashaaltay04@gmail.com>
2024-02-19 17:55:51 +0700

    first commit

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -9,100 +9,85 @@ def start():
             print(col,end=" ")
     print("")
 
-def first_move():
-    symbol_X_row = int(input(first_name+ ", введите строку "))
-    symbol_X_col = int(input(first_name+', введите столбец '))
-    matrix[symbol_X_row - 1][symbol_X_col - 1] = "X"
-    start()
-
-def second_move():
-    symbol_Y_row = int(input(second_name+', введите строку '))
-    symbol_Y_col = int(input(second_name+', введите столбец '))
-    matrix[symbol_Y_row - 1][symbol_Y_col - 1] = "0"
-    start()
-
-def win0():
-    if matrix[0][0] and matrix[0][1] and matrix[0][2] == "0":
-        print("Победил "+second_name)
-    elif matrix[1][0] and matrix[1][1] and matrix[1][2] == "0":
-        print("Победил "+second_name)
-    elif matrix[2][0] and matrix[2][1] and matrix[2][2] == "0":
-        print("Победил"+second_name)
-    elif matrix[0][0] and matrix[0][1] and matrix[0][2] == "0":
-        print("Победил "+cond_name)
-    elif matrix[0][1] and matrix[1][1] and matrix[2][1] == "0":
-        print("Победил "+second_name)
-    elif matrix[0][2] and matrix[1][2] and matrix[2][2] =="0":
-        print("Победил "+second_name)
-    elif matrix[0][0] and matrix[1][1] and matrix[2][2] == "0":
-        print("Победил "+second_name)
-    elif matrix[2][0] and matrix[1][1] and matrix[0][2] == "0":
-        print("Победил "+second_name) 
-
-def winx():
-    if matrix[0][0] and matrix[0][1] and matrix[0][2] == "x":
-        print("Победил "+a)
-    elif matrix[1][0] and matrix[1][1] and matrix[1][2] == "x":
-        print("Победил "+a)
-    elif matrix[2][0] and matrix[2][1] and matrix[2][2] == "x":
-        print("Победил"+a)
-    elif matrix[0][0] and matrix[1][0] and matrix[2][0]  == "x":
-        print("Победил "+a)
-    elif matrix[0][1] and matrix[1][1] and matrix[2][1] == "x":
-        print("Победил "+a)
-    elif matrix[0][2] and matrix[1][2] and matrix[2][2] =="x":
-        print("Победил "+a)
-    elif matrix[0][0] and matrix[1][1] and matrix[2][2] == "x":
-        print("Победил "+a)
-    elif matrix[2][0] and matrix[1][1] and matrix[0][2] == "x":
-        print("Победил "+a)
-
-
-
-
-
 first_name = str(input('Введите имя первого игрока '))
 second_name = str(input('Введите имя второго игрока '))
 
-active = True
+#начало игры 
 while True:
+#первый ход
     symbol_X_row = int(input(first_name+ ", введите строку "))
+    if symbol_X_row == 111:
+        print("Игра закончена")
+        break
     symbol_X_col = int(input(first_name+', введите столбец '))
+    if symbol_X_col == 111:
+        print("Ирга закончена")
+        break
     matrix[symbol_X_row - 1][symbol_X_col - 1] = "X"
     start()
-    if matrix[0][0] and matrix[0][1] and matrix[0][2] == "x":
+    if matrix[0][0] == "X" and matrix[0][1] == "X" and matrix[0][2] == "X":
+        print("Победил "+first_name+"!")
         break
+    if matrix[1][0] == "X" and matrix[1][1] == "X" and matrix[1][2] == "X":
         print("Победил "+first_name+"!")
-    elif matrix[1][0] and matrix[1][1] and matrix[1][2] == "x":
-        active = False
+        break
+    if matrix[2][0] == "X" and matrix[2][1] == "X" and matrix[2][2] == "X":
         print("Победил "+first_name+"!")
-    elif matrix[2][0] and matrix[2][1] and matrix[2][2] == "x":
-        active = False
+        break
+    if matrix[0][0] == "X" and matrix[1][0] == "X" and matrix[2][0] == "X":
         print("Победил "+first_name+"!")
-    elif matrix[0][0] and matrix[1][0] and matrix[2][0]  == "x":
-        active = False
+        break
+    if matrix[0][1] == "X" and matrix[1][1] == "X" and matrix[2][1] == "X":
         print("Победил "+first_name+"!")
-    elif matrix[0][1] and matrix[1][1] and matrix[2][1] == "x":
-        active = False
+        break 
+    if matrix[0][2] == "X" and matrix[1][2] == "X" and matrix[2][2] =="X":
         print("Победил "+first_name+"!")
-    elif matrix[0][2] and matrix[1][2] and matrix[2][2] =="x":
-        active = False
+        break
+    if matrix[0][0] == "X" and matrix[1][1] == "X" and matrix[2][2] == "X":
         print("Победил "+first_name+"!")
-    elif matrix[0][0] and matrix[1][1] and matrix[2][2] == "x":
-        active = False
+        break 
+    if matrix[2][0] == "X" and matrix[1][1] == "X" and matrix[0][2] == "X": 
         print("Победил "+first_name+"!")
-    elif matrix[2][0] and matrix[1][1] and matrix[0][2] == "x": 
-        active = False
-        print("Победил "+first_name+"!")
-    symbol_Y_row = int(input(second_name+', введите строку '))
-    symbol_Y_col = int(input(second_name+', введите столбец '))
-    matrix[symbol_Y_row - 1][symbol_Y_col - 1] = "0"
-    start()
+        break
     
-        #active = False
-        #print("Победил "+second_name+"!")
-    #if symbol_X_row == 111 or symbol_X_col == 111 or symbol_Y_row == 111 or symbol_Y_col == 111:
-     #   break
-     #  print("Игра окончена")
+#второй ход
+    symbol_Y_row = int(input(second_name+', введите строку '))
+    if symbol_Y_row == 111:
+        print("Игра окончена")
+        break 
+    symbol_Y_col = int(input(second_name+', введите столбец '))
+    if symbol_Y_col == 111:
+        break
+        print("Игра окончена") 
+    matrix[symbol_Y_row - 1][symbol_Y_col - 1] = "O"
+    start()
+    if matrix[0][0] =="O" and matrix[0][1] =="O" and matrix[0][2] == "O":
+        print("Победил " +second_name+"!")
+        break
+    if matrix[1][0] =="O" and matrix[1][1] =="O" and matrix[1][2] == "O":
+        print("Победил " +second_name+"!")
+        break
+    if matrix[2][0] =="O" and matrix[2][1] =="O" and matrix[2][2] == "O":
+        print("Победил " +second_name+ "!")
+        break
+    if matrix[0][0] =="O" and matrix[0][1] =="O" and matrix[0][2] == "O":
+        print("Победил " +second_name+"!")
+        break
+    if matrix[0][1] =="O" and matrix[1][1] =="O" and matrix[2][1] == "O":
+        print("Победил " +second_name+"!")
+        break
+    if matrix[0][2] =="O" and matrix[1][2] =="O" and matrix[2][2] == "O":
+        print("Победил " +second_name+"!")
+        break
+    if matrix[0][0] =="O" and matrix[1][1] =="O" and matrix[2][2] == "O":
+        print("Победил " +second_name+"!")
+        break
+    if matrix[2][0] =="O" and matrix[1][1] =="O" and matrix[0][2] == "O":
+        print("Победил " +second_name+"!")
+        break
+
+
+    
+    
 
 
